4dc1c8e590f980d663acfb785999a7d1f408a59c
Robin Rice <robin135191@outlook.com>
2020-04-28 21:56:17 +0000

    Done with Tuesday

--- LibraryApi/Controllers/StatusController.cs
+++ LibraryApi/Controllers/StatusController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LibraryApi.Services;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,21 +7,69 @@ using System.Threading.Tasks;
 
 namespace LibraryApi.Controllers
 {
-    public class StatusController: Controller
+    public class StatusController : Controller
     {
+
+        ISystemTime systemTime;
+
+        public StatusController(ISystemTime systemTime)
+        {
+            this.systemTime = systemTime;
+        }
+
+
         // Get /status -> 200 Ok
         [HttpGet("/status")]
         public ActionResult GetTheStatus()
         {
             var response = new GetStatusResponse
             {
-                Message = "Everything si golden!",
+                Message = "Everything is golden!",
                 CheckedBy = "Joe",
-                WhenLastChecked = DateTime.Now
+                WhenLastChecked = systemTime.GetCurrent()
             };
             return Ok(response);
         }
+
+        //Get /employees/123/salary     //these are called "constraints"
+        [HttpGet("employees/{employeeId:int:min(1)}/salary")]
+        public ActionResult GetEmployeeSalary(int employeeId)
+        {
+            return Ok($"The Employee {employeeId} has a salary of $72,000.00");
+        }
+
+        //Get /employees?dept=DEV
+        [HttpGet("employees")]
+        public ActionResult GetEmployees([FromQuery]string dept = "All")
+        {
+            return Ok($"Returning Employees for department {dept}");
+        }
+
+        [HttpGet("whoami")]
+        public ActionResult WhoAmi(
+            [FromHeader(Name = "User-Agent")]
+            string userAgent)
+        {
+            return Ok($"I see you are running {userAgent}");
+        }
+
+        [HttpPost("employees")]
+        public ActionResult HireEmployee([FromBody] EmployeeCreateRequest employeeToHire)
+        {
+            return Ok($"Hiring {employeeToHire.LastName} " +
+                $"as a {employeeToHire.Department}");
+        }
+
     }
+
+
+    public class EmployeeCreateRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+    }
+
 
     public class GetStatusResponse
     {
---
+++ LibraryApiIntegrationTest/TestSystemTime.cs
@@ -0,0 +1,6 @@
+﻿namespace LibraryApiIntegrationTests
+{
+    internal class TestSystemTime
+    {
+    }
+}
